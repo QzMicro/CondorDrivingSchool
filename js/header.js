533a45c8a2af887aa39bdeb391233325e961efa3
qzmicro <qzmicro@hotmail.com>
2020-01-10 22:15:39 -0800

    This is the new repository.  Work in progress...

---
+++ js/header.js
@@ -0,0 +1,40 @@
+
+document.write('<meta charset="UTF-8">\
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">\
+<meta name="apple-mobile-web-app-capable" content="yes">\
+<meta name="description" content="Condor Driving and Traffic School">\
+<meta name="theme-color" content="#13294b">\
+<meta property="og:type" content="website">\
+<meta property="og:url" content="">\
+<meta property="og:title" content="Public driving and traffic school | Condor">\
+<meta property="og:image" content="img/socialog.jpg">\
+<meta property="og:description" content="Public driving and traffic school | Condor">\
+<meta property="og:site_name" content="CondorDrivingSchool">\
+<meta property="og:locale" content="en_US">\
+<meta name="twitter:card" content="summary">\
+<meta name="twitter:site" content="@CondorDriving">\
+<meta name="twitter:creator" content="@CondorDriving">\
+<meta name="twitter:url" content="">\
+<meta name="twitter:title" content="Public driving and traffic school | Condor">\
+<meta name="twitter:description" content="Home">\
+<meta name="twitter:image" content="img/socialog.jpg">\
+<link rel="shortcut icon" href="img/fav/favicon.png" type="image/x-icon">\
+<link rel="icon" type="image/png" href="img/fav/speeddial-160px.png">\
+<link rel="icon" type="image/x-icon" href="img/fav/favicon.png">\
+<link rel="apple-touch-icon" sizes="114x114" href="img/fav/apple-touch-icon-114x114-precomposed.png">\
+<link rel="apple-touch-icon" sizes="72x72" href="img/fav/apple-touch-icon-72x72-precomposed.png">\
+<link rel="apple-touch-icon" href="img/fav/apple-touch-icon-57x57-precomposed.png">\
+<link rel="stylesheet" id="contact-form-7-css"  href="css/contact-form-7/styles.css" type="text/css" media="all">\
+<link rel="stylesheet" id="main-style-css"  href="css/style.css" type="text/css" media="all">\
+<link rel="stylesheet" id="louis-style-css"  href="css/style-louis.css" type="text/css" media="all">\
+<link refl"stylesheet" id="izi-modal-css" href="css/izimodal.min.css" type="text/css" media="all">\
+<link rel="https://api.w.org/" href="wp-json/">\
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc.php">\
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml">\
+<link rel="canonical" href="">\
+<link rel="shortlink" href="">\
+<link rel="alternate" type="application/json+oembed" href="js/embed">\
+<link rel="stylesheet" href="css/izimodal.css">\
+<link rel="stylesheet" href="css/modal-logon.css">\
+<link rel="stylesheet" href="css/icomoon.css">\
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,300,700">');
---
+++ js/plugins.js
@@ -0,0 +1,13 @@
+
+document.write("<script type='text/javascript' src='js/jquery-3.2.1.min.js'></script>\
+<script type='text/javascript' src='js/contact-form-7/scripts.js'></script>\
+<script type='text/javascript' src='js/pace.min.js'></script>\
+<script type='text/javascript' src='js/modernizr-custom.js'></script>\
+<script type='text/javascript' src='js/barba.min.js'></script>\
+<script type='text/javascript' src='js/tweenmax.min.js'></script>\
+<script type='text/javascript' src='js/scrolltoplugin.min.js'></script>\
+<script type='text/javascript' src='js/jquery.shave.min.js'></script>\
+<script type='text/javascript' src='js/mobile-chrome-vh-fix.js'></script>\
+<script type='text/javascript' src='js/izimodal.js'></script>\
+<script type='text/javascript' src='js/script.js'></script>\
+<script type='text/javascript' src='js/wp-embed.min.js'></script>");
